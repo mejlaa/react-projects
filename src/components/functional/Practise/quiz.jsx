75d3804bac4bc4586c09e4c7c07449d1d44e5eb2
Mejla Ugljanin <ugljanin.mejla@hotmail.com>
2022-10-10 13:44:09 +0200

    delete files

--- src/components/functional/Practise/quiz.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Quiz1 = () => {
-  return <div></div>;
-};
-
-export default Quiz1;
--- src/components/functional/UserLoginApp/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LoginLogout = () => {
-  return <div></div>;
-};
-
-export default LoginLogout;
